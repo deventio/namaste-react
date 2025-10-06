4a3ff9db47cc7545e1922efeba9de71ba8d763ae
Upasana Sharma <73016023+deventio@users.noreply.github.com>
2025-10-06 13:24:21 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div", 
-    {id:"parent"},  
-    [React.createElement("div", {id:"child"}, 
-       [React.createElement("h1", {}, "I am H1 tag!"), 
-        React.createElement("h2", {}, "I am H2 tag!")]
+    {id:"parent", key: 5},  
+    [React.createElement("div", {id:"child", key: 7 }, 
+       [React.createElement("h1", { key: 1}, "I am H1 tag!"), 
+        React.createElement("h2", {key: 2}, "I am H2 tag!")]
     ), 
-    React.createElement("div", {id:"child2"}, 
-       [React.createElement("h1", {}, "I am H1 tag!"), 
-        React.createElement("h2", {}, "I am H2 tag!")]
+    React.createElement("div", {id:"child2", key: 8 }, 
+       [React.createElement("h1", {key: 3}, "I am H1 tag!"), 
+        React.createElement("h2", {key: 4}, "I am H2 tag!")]
     )
     ]);
 
 
 const heading = React.createElement("h1", {
 id: "heading",
-xyz : "abc"
+xyz : "abc",
+key: 6
 
 }, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
