54d0aad93e3203f7d6f3658ec8b217de218af539
Upasana Sharma <73016023+deventio@users.noreply.github.com>
2025-10-13 16:55:22 +0530

    episode-06.a-call Swiggy api directly from app and render data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,150 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resObj from "../utils/mockData";
 
 const Body = () => {
   //  Local State Variable - Super powerful variable
   const [listOfRestaurants, setListOfRestaurants] = useState(resObj);
 
-  //Normal JS Variable
-  //let listOfRestaurants = null;
+  useEffect(() => {
+    console.log("useeffect called");
+    fetchData();
+  }, []);
 
-  //Normal JS Variable
-  let listOfRestaurantsJS = [
-    {
-      info: {
-        id: "393840",
-        name: "Chinese Wok",
-        cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
-        locality: "Chikka Lakshmaiah Layout",
-        areaName: "Adugodi",
-        costForTwo: "₹250 for two",
-        cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
-        avgRating: 3.2,
-        parentId: "61955",
-        avgRatingString: "4.3",
-        totalRatingsString: "6.2K+",
-        sla: {
-          deliveryTime: 35,
-          lastMileTravel: 2.5,
-          serviceability: "SERVICEABLE",
-          slaString: "35-40 mins",
-          lastMileTravelString: "2.5 km",
-          iconType: "ICON_TYPE_EMPTY",
-        },
-        availability: {
-          nextCloseTime: "2025-10-08 02:00:00",
-          opened: true,
-        },
-        badges: {},
-        isOpen: true,
-        type: "F",
-        badgesV2: {
-          entityBadges: {
-            imageBased: {},
-            textBased: {},
-            textExtendedBadges: {},
-          },
-        },
-        aggregatedDiscountInfoV3: {
-          header: "ITEMS",
-          subHeader: "AT ₹119",
-        },
-        differentiatedUi: {
-          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-          differentiatedUiMediaDetails: {
-            lottie: {},
-            video: {},
-          },
-        },
-        reviewsSummary: {},
-        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-        restaurantOfferPresentationInfo: {},
-        externalRatings: {
-          aggregatedRating: {
-            rating: "--",
-          },
-        },
-        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
-      },
-      analytics: {
-        context: "seo-data-9903825e-1cfc-42fa-9026-cc7388c67674",
-      },
-      cta: {
-        link: "https://www.swiggy.com/city/bangalore/chinese-wok-chikka-lakshmaiah-layout-adugodi-rest393840",
-        type: "WEBLINK",
-      },
-    },
-    {
-      info: {
-        id: "10576",
-        name: "Pizza Hut",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/bb48c990-87ae-4884-83d8-03d73f3cd2a7_10576.JPG",
-        locality: "6th Block",
-        areaName: "Koramangala",
-        costForTwo: "₹350 for two",
-        cuisines: ["Pizzas"],
-        avgRating: 4.3,
-        parentId: "721",
-        avgRatingString: "4.3",
-        totalRatingsString: "21K+",
-        sla: {
-          deliveryTime: 25,
-          lastMileTravel: 0.7,
-          serviceability: "SERVICEABLE",
-          slaString: "20-25 mins",
-          lastMileTravelString: "0.7 km",
-          iconType: "ICON_TYPE_EMPTY",
-        },
-        availability: {
-          nextCloseTime: "2025-10-08 05:00:00",
-          opened: true,
-        },
-        badges: {},
-        isOpen: true,
-        type: "F",
-        badgesV2: {
-          entityBadges: {
-            imageBased: {},
-            textBased: {},
-            textExtendedBadges: {},
-          },
-        },
-        aggregatedDiscountInfoV3: {
-          header: "ITEMS",
-          subHeader: "AT ₹98",
-        },
-        differentiatedUi: {
-          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-          differentiatedUiMediaDetails: {
-            lottie: {},
-            video: {},
-          },
-        },
-        reviewsSummary: {},
-        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-        restaurantOfferPresentationInfo: {},
-        externalRatings: {
-          aggregatedRating: {
-            rating: "4.6",
-            ratingCount: "3.6K+",
-          },
-          source: "GOOGLE",
-          sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
-        },
-        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
-      },
-      analytics: {
-        context: "seo-data-9903825e-1cfc-42fa-9026-cc7388c67674",
-      },
-      cta: {
-        link: "https://www.swiggy.com/city/bangalore/pizza-hut-6th-block-koramangala-rest10576",
-        type: "WEBLINK",
-      },
-    },
-  ];
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+    console.log(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+
+    //optional chaining
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    ); /*
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );*/
+  };
+
+  console.log("body rendered");
 
   return (
     <div className="body">
