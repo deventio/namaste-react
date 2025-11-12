b77ca17403bc0b559691bffa2596a6a5cc1edb6a
deventio <73016023+deventio@users.noreply.github.com>
2025-11-12 14:26:26 +0530

    episode-08.c-state variables in components

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,13 +3,20 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         console.log(props);
-
+        this.state={
+            count: 0,
+            count2: 2
+        }
     }
     render(){
+
+        const {name, location} = this.props;
         return( 
             <div className="user-card">
-            <h2>Name: {this.props.name}</h2>
-            <h3>Location: {this.props.location}</h3>
+                <h1>Count: {this.state.count}</h1>
+                  <h1>Count: {this.state.count2}</h1>
+            <h2>Name: {name}</h2>
+            <h3>Location: {location}</h3>
             <h2>Social: Twitter</h2>
             </div>
         );
